0d06d3a9f75832c8fce1820139b69757d79d0c83
danghieu1407 <danghieu140701@gmail.com>
2024-10-24 15:56:23 +0700

    MDL-60910 ddimageortext: Create option for using transparent pictures

--- question/type/ddimageortext/lang/en/qtype_ddimageortext.php
+++ question/type/ddimageortext/lang/en/qtype_ddimageortext.php
@@ -76,5 +76,6 @@ $string['summarisechoice'] = '{$a->no}. {$a->text}';
 $string['summariseplace'] = '{$a->no}. {$a->text}';
 $string['summarisechoiceno'] = 'Item {$a}';
 $string['summariseplaceno'] = 'Drop zone {$a}';
+$string['transparentdropzone'] = 'Make drop zone boxes transparent';
 $string['xleft'] = 'Left';
 $string['ytop'] = 'Top';
